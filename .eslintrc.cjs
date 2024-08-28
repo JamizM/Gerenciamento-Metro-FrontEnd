adc384c89408623e63ac35ae0bf8490bb1ceef78
Gustavo Ara <gustavo.ara2005.p@gmail.com>
2024-08-28 10:17:07 -0300

    Test Husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2021: true, node: true },
+    extends: [
+        "eslint:recommended",
+        "universe/native",
+        "plugin:prettier/recommended"
+    ],
+    ignorePatterns: ["dist", ".eslint.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react", "react-native"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    rules: {
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto"
+            }
+        ]
+    }
+};
