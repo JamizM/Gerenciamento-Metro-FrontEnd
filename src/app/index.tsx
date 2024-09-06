9e271218ec8418b93e9c67f8dd803dcee775cf5c
Gustavo Ara <23.00791-5@maua.br>
2024-09-05 23:06:46 -0300

    configuring expo-route

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import InitialPage from "./screen/InitialPage";
+
+export default function Home() {
+    return (
+        <View>
+            <Text>Initial Page</Text>
+        </View>
+    );
+}
