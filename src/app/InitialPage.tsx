ca1a95ee793537f12ceaf50c3ef7b476fd0f52f9
Gustavo Ara <23.00791-5@maua.br>
2024-09-08 16:38:56 -0300

    Input system creation

--- src/app/InitialPage.tsx
+++ src/app/InitialPage.tsx
@@ -1,21 +1,54 @@
-import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
+import React from "react";
+import { View, StyleSheet, SafeAreaView, Image } from "react-native";
+import { TextInput } from "react-native-paper";
+
+import MetroSP from "./images/metro-sp";
 
 export default function InitialPage() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
     return (
-        <View style={{ backgroundColor: "#2c53a1", flex: 1 }}>
-            <View style={styles.container} />
+        <View style={styles.outerContainer}>
+            <View style={styles.container}>
+                <MetroSP />
+                <SafeAreaView>
+                    <TextInput
+                        mode="outlined"
+                        label="Email"
+                        style={styles.textInput}
+                        value={email}
+                        onChangeText={(email) => setEmail(email)}
+                    />
+                    <TextInput
+                        mode="outlined"
+                        label="Senha"
+                        style={styles.textInput}
+                        value={password}
+                        onChangeText={(password) => setPassword(password)}
+                    />
+                </SafeAreaView>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    outerContainer: {
+        backgroundColor: "#e2dad8",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
     container: {
         backgroundColor: "white",
-        height: 350,
-        width: 350,
-        alignSelf: "center",
+        height: 370,
+        width: 370,
         justifyContent: "center",
         borderRadius: 35
+    },
+    textInput: {
+        height: 50,
+        margin: 15
     }
 });
