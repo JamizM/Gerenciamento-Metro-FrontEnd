c02aa8ad1eda255b29605e296d9d3935776d7895
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-10-01 19:05:43 -0300

    Qr Code reader first v

---
+++ src/app/camera/Camera.tsx
@@ -0,0 +1,32 @@
+import { BrowserMultiFormatReader } from "@zxing/library";
+import { useRef, useCallback } from "react";
+import Webcam from "react-webcam";
+
+export default function QRCodeScanner() {
+    const webcamRef = useRef<Webcam>(null);
+
+    const scanQRCode = useCallback(() => {
+        const codeReader = new BrowserMultiFormatReader();
+        if (webcamRef.current) {
+            codeReader.decodeFromVideoDevice(
+                null,
+                webcamRef.current.video!,
+                (result: { getText: () => any }, err: any) => {
+                    if (result) {
+                        console.log(result.getText());
+                    }
+                    if (err) {
+                        console.error(err);
+                    }
+                }
+            );
+        }
+    }, [webcamRef]);
+
+    return (
+        <div>
+            <Webcam ref={webcamRef} />
+            <button onClick={scanQRCode}>Scanear o QR Code</button>
+        </div>
+    );
+}
