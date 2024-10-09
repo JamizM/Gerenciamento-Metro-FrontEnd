20ee242b22fbeebc2d07870e38434fe18e034344
VictorCodinhoto <23.00051-0@maua.br>
2024-10-09 17:43:13 -0300

    Get requisition working with Axios lib(Role Authentication)

--- api/PuxarExtintoresPorEstacao.ts
+++ api/PuxarExtintoresPorEstacao.ts
@@ -20,15 +20,31 @@ interface Extinguisher {
 
     localization: string; // Relacionamento ManyToOne com Localization
 }
+interface ExtinguisherResponse {
+    _embedded: {
+        extinguishers: Extinguisher[];
+    };
+}
 // Função que recebe como parâmetro o id da localização no banco, ele puxa da api com o url parametrizado e devolve todos os dados, ou um erro,
 // então, após puxar os dados, tratá-los
-export default async function PuxarExtintoresPorEstacao(localizacao: String) {
-    const url = `http://localhost:8080/api/Extinguishers?localization_id=${localizacao}`;
+export default async function PuxarExtintoresPorEstacao() {
+    const base64Credentials = btoa("Admin:Admin");
+    const url = `http://localhost:8080/api/Extinguishers/`;
     try {
-        const response = await axios.get<Extinguisher[]>(url);
-        return response.data.length;
+        const response = await axios.get<ExtinguisherResponse>(url, {
+            headers: {
+                Authorization: `Basic ${base64Credentials}`,
+                "Content-Type": "application/json"
+            }
+        });
+
+        return response.data._embedded.extinguishers.length;
     } catch (error) {
         console.error("Erro ao buscar extintores:", error);
         return 0;
     }
 }
+
+PuxarExtintoresPorEstacao().then((result) => {
+    console.log("Quantidade de extintores:", result);
+});
