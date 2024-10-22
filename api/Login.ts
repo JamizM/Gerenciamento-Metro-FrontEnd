f7c566dfd9b1af66695618088e8f72b5098dfae5
VictorCodinhoto <23.00051-0@maua.br>
2024-10-22 18:44:54 -0300

    Login working

---
+++ api/Login.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+interface User {
+    id: number;
+    firstName: string;
+    email: string;
+    password?: string;
+}
+export default async function Login(emailUser: string, passwordUser: string) {
+    const url = `http://localhost:8080/api/Users?email=${emailUser}`;
+    const base64Credentials = btoa("Admin:Admin");
+
+    try {
+        const response = await axios.get<User[]>(url, {
+            headers: {
+                Authorization: `Basic ${base64Credentials}`,
+                "Content-Type": "application/json"
+            }
+        });
+
+        const user = response.data[0];
+
+        if (user) {
+            const password = user.password;
+            if (password == passwordUser) {
+                console.log("Login bem-sucedido!");
+                return true;
+            } else {
+                console.log("Senha incorreta.");
+                return false;
+            }
+        } else {
+            console.log("Usuário não encontrado.");
+            return false;
+        }
+    } catch (error) {
+        console.error("Erro ao buscar Usuário:", error);
+        return false;
+    }
+}
